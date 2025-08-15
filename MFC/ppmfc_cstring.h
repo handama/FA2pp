96676a0b048e14f89e98e67ca832e317129309f5
handama <55939089+handama@users.noreply.github.com>
2025-08-16 00:31:17 +0800

    rewrite format

--- MFC/ppmfc_cstring.h
+++ MFC/ppmfc_cstring.h
@@ -1,6 +1,18 @@
 #pragma once
 
 #include "ppmfc_forward.h"
+#include <string>
+#include <algorithm>
+#include <cctype>
+#include <cstdarg>
+#include <cstdio>
+#include <stdexcept>
+#include <vector>
+#include <type_traits>
+#include <utility>
+#include <variant>
+
+class FString;
 
 _PPMFC_BEGIN
 
@@ -187,6 +199,16 @@ public:
 	bool operator!= (const CString& another) const
 		{ return !(*this == another); }
 
+    bool operator== (const std::string& another) const
+    {
+        return strcmp(this->m_pchData, another.c_str()) == 0;
+    }
+
+    bool operator!= (const std::string& another) const
+    {
+        return !(*this == another);
+    }
+
 	bool operator< (const char* another) const
         { return strcmp(this->m_pchData, another) < 0; }
 
@@ -267,265 +289,258 @@ private:
 		JMP_STD(0x43D960);
 	}
 
-	void FormatV(LPCTSTR lpszFormat, va_list argList)
-	{
-		constexpr int FORCE_ANSI = 0x10000;
-		constexpr int FORCE_UNICODE = 0x20000;
-
-		ASSERT(AfxIsValidString(lpszFormat, FALSE));
-
-		va_list argListSave = argList;
-
-		// make a guess at the maximum length of the resulting string
-		int nMaxLen = 0;
-		for (LPCTSTR lpsz = lpszFormat; *lpsz != '\0'; lpsz = _tcsinc(lpsz))
-		{
-			// handle '%' character, but watch out for '%%'
-			if (*lpsz != '%' || *(lpsz = _tcsinc(lpsz)) == '%')
-			{
-				nMaxLen += _tclen(lpsz);
-				continue;
-			}
-
-			int nItemLen = 0;
-
-			// handle '%' character with format
-			int nWidth = 0;
-			for (; *lpsz != '\0'; lpsz = _tcsinc(lpsz))
-			{
-				// check for valid flags
-				if (*lpsz == '#')
-					nMaxLen += 2;   // for '0x'
-				else if (*lpsz == '*')
-					nWidth = va_arg(argList, int);
-				else if (*lpsz == '-' || *lpsz == '+' || *lpsz == '0' ||
-					*lpsz == ' ')
-					;
-				else // hit non-flag character
-					break;
-			}
-			// get width and skip it
-			if (nWidth == 0)
-			{
-				// width indicated by
-				nWidth = _ttoi(lpsz);
-				for (; *lpsz != '\0' && _istdigit(*lpsz); lpsz = _tcsinc(lpsz))
-					;
-			}
-			ASSERT(nWidth >= 0);
-
-			int nPrecision = 0;
-			if (*lpsz == '.')
-			{
-				// skip past '.' separator (width.precision)
-				lpsz = _tcsinc(lpsz);
-
-				// get precision and skip it
-				if (*lpsz == '*')
-				{
-					nPrecision = va_arg(argList, int);
-					lpsz = _tcsinc(lpsz);
-				}
-				else
-				{
-					nPrecision = _ttoi(lpsz);
-					for (; *lpsz != '\0' && _istdigit(*lpsz); lpsz = _tcsinc(lpsz))
-						;
-				}
-				ASSERT(nPrecision >= 0);
-			}
-
-			// should be on type modifier or specifier
-			int nModifier = 0;
-			switch (*lpsz)
-			{
-				// modifiers that affect size
-			case 'h':
-				nModifier = FORCE_ANSI;
-				lpsz = _tcsinc(lpsz);
-				break;
-			case 'l':
-				nModifier = FORCE_UNICODE;
-				lpsz = _tcsinc(lpsz);
-				break;
-
-				// modifiers that do not affect size
-			case 'F':
-			case 'N':
-			case 'L':
-				lpsz = _tcsinc(lpsz);
-				break;
-			}
-
-			// now should be on specifier
-			switch (*lpsz | nModifier)
-			{
-				// single characters
-			case 'c':
-			case 'C':
-				nItemLen = 2;
-				va_arg(argList, TCHAR);
-				break;
-			case 'c' | FORCE_ANSI:
-			case 'C' | FORCE_ANSI:
-				nItemLen = 2;
-				va_arg(argList, char);
-				break;
-			case 'c' | FORCE_UNICODE:
-			case 'C' | FORCE_UNICODE:
-				nItemLen = 2;
-				va_arg(argList, wchar_t);
-				break;
-
-				// strings
-			case 's':
-			{
-				LPCTSTR pstrNextArg = va_arg(argList, LPCTSTR);
-				if (pstrNextArg == nullptr)
-					nItemLen = 6;  // "(null)"
-				else
-				{
-					nItemLen = lstrlen(pstrNextArg);
-					nItemLen = MAX(1, nItemLen);
-				}
-				break;
-			}
-
-			case 'S':
-			{
-#ifndef _UNICODE
-				LPWSTR pstrNextArg = va_arg(argList, LPWSTR);
-				if (pstrNextArg == NULL)
-					nItemLen = 6;  // "(null)"
-				else
-				{
-					nItemLen = wcslen(pstrNextArg);
-					nItemLen = MAX(1, nItemLen);
-				}
-#else
-				LPCSTR pstrNextArg = va_arg(argList, LPCSTR);
-				if (pstrNextArg == NULL)
-					nItemLen = 6; // "(null)"
-				else
-				{
-					nItemLen = lstrlenA(pstrNextArg);
-					nItemLen = max(1, nItemLen);
-				}
-#endif
-				break;
-			}
-
-			case 's' | FORCE_ANSI:
-			case 'S' | FORCE_ANSI:
-			{
-				LPCSTR pstrNextArg = va_arg(argList, LPCSTR);
-				if (pstrNextArg == NULL)
-					nItemLen = 6; // "(null)"
-				else
-				{
-					nItemLen = lstrlenA(pstrNextArg);
-					nItemLen = MAX(1, nItemLen);
-				}
-				break;
-			}
-
-#ifndef _MAC
-			case 's' | FORCE_UNICODE:
-			case 'S' | FORCE_UNICODE:
-			{
-				LPWSTR pstrNextArg = va_arg(argList, LPWSTR);
-				if (pstrNextArg == NULL)
-					nItemLen = 6; // "(null)"
-				else
-				{
-					nItemLen = wcslen(pstrNextArg);
-					nItemLen = MAX(1, nItemLen);
-				}
-				break;
-			}
-#endif
-			}
-
-			// adjust nItemLen for strings
-			if (nItemLen != 0)
-			{
-				nItemLen = MAX(nItemLen, nWidth);
-				if (nPrecision != 0)
-					nItemLen = MIN(nItemLen, nPrecision);
-			}
-			else
-			{
-				switch (*lpsz)
-				{
-					// integers
-				case 'd':
-				case 'i':
-				case 'u':
-				case 'x':
-				case 'X':
-				case 'o':
-					va_arg(argList, int);
-					nItemLen = 32;
-					nItemLen = MAX(nItemLen, nWidth + nPrecision);
-					break;
-
-				case 'e':
-				case 'f':
-				case 'g':
-				case 'G':
-					va_arg(argList, double);
-					nItemLen = 128;
-					nItemLen = MAX(nItemLen, nWidth + nPrecision);
-					break;
-
-				case 'p':
-					va_arg(argList, void*);
-					nItemLen = 32;
-					nItemLen = MAX(nItemLen, nWidth + nPrecision);
-					break;
-
-					// no output
-				case 'n':
-					va_arg(argList, int*);
-					break;
-
-				default:
-					ASSERT(FALSE);  // unknown formatting option
-				}
-			}
-
-			// adjust nMaxLen for output nItemLen
-			nMaxLen += nItemLen;
-		}
-
-		GetBuffer(nMaxLen);
-		VERIFY(_vstprintf(m_pchData, lpszFormat, argListSave) <= GetAllocLength());
-		ReleaseBuffer();
-
-		va_end(argListSave);
-	}
-public:
-    void AFX_CDECL Format(LPCTSTR lpszFormat, ...)
-    {
-        ASSERT(AfxIsValidString(lpszFormat, FALSE));
-
-        va_list argList;
-        va_start(argList, lpszFormat);
-        FormatV(lpszFormat, argList);
-        va_end(argList);
+    template<typename T>
+    void convertArg(std::vector<std::variant<int, double, const char*, const wchar_t*>>& vec, T&& arg) {
+        using U = std::decay_t<T>;
+        if constexpr (std::is_same_v<U, FString>) {
+            vec.push_back(arg.c_str());
+        }
+        else if constexpr (std::is_same_v<U, CString>) {
+            vec.push_back(arg.m_pchData);
+        }
+        else if constexpr (std::is_same_v<U, const char*>) {
+            vec.push_back(arg);
+        }
+        else if constexpr (std::is_same_v<U, char*>) {
+            vec.push_back(static_cast<const char*>(arg));
+        }
+        else if constexpr (std::is_same_v<U, const wchar_t*>) {
+            vec.push_back(arg);
+        }
+        else if constexpr (std::is_same_v<U, int>) {
+            vec.push_back(arg);
+        }
+        else if constexpr (std::is_same_v<U, size_t>) {
+            vec.push_back(static_cast<int>(arg));
+        }
+        else if constexpr (std::is_same_v<U, short>) {
+            vec.push_back(static_cast<int>(arg));
+        }
+        else if constexpr (std::is_same_v<U, word>) {
+            vec.push_back(static_cast<int>(arg));
+        }
+        else if constexpr (std::is_same_v<U, char>) {
+            vec.push_back(static_cast<int>(arg));
+        }
+        else if constexpr (std::is_same_v<U, byte>) {
+            vec.push_back(static_cast<int>(arg));
+        }
+        else if constexpr (std::is_same_v<U, double>) {
+            vec.push_back(arg);
+        }
+        else if constexpr (std::is_same_v<U, float>) {
+            vec.push_back(static_cast<double>(arg));
+        }
+        else if constexpr (std::is_same_v<U, void*>) {
+            vec.push_back(reinterpret_cast<intptr_t>(arg));
+        }
+        else if constexpr (std::is_same_v<U, int*>) {
+            vec.push_back(reinterpret_cast<intptr_t>(arg));
+        }
+        else {
+            static_assert(false, "Unsupported argument type for Format");
+        }
     }
 
-    void AFX_CDECL Format(UINT nFormatID, ...)
-    {
+    void FormatVImpl(const std::string& format, const std::vector<std::variant<int, double, const char*, const wchar_t*>>& args) {
+        constexpr int FORCE_ANSI = 0x10000;
+        constexpr int FORCE_UNICODE = 0x20000;
+
+        size_t argIndex = 0;
+        std::string result;
+        result.reserve(format.length() + 32);
+
+        for (const char* p = format.c_str(); *p; ++p) {
+            if (*p != '%') {
+                result += *p;
+                continue;
+            }
+
+            ++p;
+            if (*p == '%') {
+                result += '%';
+                continue;
+            }
+
+            std::string formatSpec = "%";
+            int nWidth = 0;
+            int nPrecision = 0;
+            int nModifier = 0;
+
+            while (*p == '#' || *p == '*' || *p == '-' || *p == '+' || *p == '0' || *p == ' ') {
+                formatSpec += *p;
+                if (*p == '*') {
+                    if (argIndex >= args.size()) throw std::runtime_error("Too few arguments for width");
+                    nWidth = std::get<int>(args[argIndex++]);
+                }
+                ++p;
+            }
+
+            if (isdigit(*p)) {
+                formatSpec += std::string(p, strspn(p, "0123456789"));
+                nWidth = atoi(p);
+                while (isdigit(*p)) ++p;
+            }
+
+            if (*p == '.') {
+                formatSpec += '.';
+                ++p;
+                if (*p == '*') {
+                    formatSpec += '*';
+                    if (argIndex >= args.size()) throw std::runtime_error("Too few arguments for precision");
+                    nPrecision = std::get<int>(args[argIndex++]);
+                    ++p;
+                }
+                else if (isdigit(*p)) {
+                    formatSpec += std::string(p, strspn(p, "0123456789"));
+                    nPrecision = atoi(p);
+                    while (isdigit(*p)) ++p;
+                }
+            }
+
+            switch (*p) {
+            case 'h':
+                nModifier = FORCE_ANSI;
+                formatSpec += 'h';
+                ++p;
+                break;
+            case 'l':
+                nModifier = FORCE_UNICODE;
+                formatSpec += 'l';
+                ++p;
+                break;
+            case 'F':
+            case 'N':
+            case 'L':
+                formatSpec += *p;
+                ++p;
+                break;
+            }
+
+            if (argIndex >= args.size()) throw std::runtime_error("Too few arguments for format specifier");
+            formatSpec += *p;
+
+            switch (*p | nModifier) {
+            case 'c':
+            case 'C':
+            case 'c' | FORCE_ANSI:
+            case 'C' | FORCE_ANSI:
+            {
+                int value = std::get<int>(args[argIndex++]);
+                char buf[16];
+                snprintf(buf, sizeof(buf), formatSpec.c_str(), value);
+                result += buf;
+                break;
+            }
+            case 'c' | FORCE_UNICODE:
+            case 'C' | FORCE_UNICODE:
+            {
+                wchar_t value = static_cast<wchar_t>(std::get<int>(args[argIndex++]));
+                char buf[16];
+                snprintf(buf, sizeof(buf), "%lc", value);
+                result += buf;
+                break;
+            }
+            case 's':
+            case 's' | FORCE_ANSI:
+            case 'S':
+            case 'S' | FORCE_ANSI:
+            {
+                const char* str = std::get<const char*>(args[argIndex++]);
+                char buf[1024];
+                snprintf(buf, sizeof(buf), formatSpec.c_str(), str ? str : "(null)");
+                result += buf;
+                break;
+            }
+            case 's' | FORCE_UNICODE:
+            case 'S' | FORCE_UNICODE:
+            {
+                const wchar_t* str = std::get<const wchar_t*>(args[argIndex++]);
+                char buf[1024];
+                if (str) {
+                    std::wstring ws(str);
+                    std::string s(ws.begin(), ws.end());
+                    snprintf(buf, sizeof(buf), formatSpec.c_str(), s.c_str());
+                }
+                else {
+                    snprintf(buf, sizeof(buf), formatSpec.c_str(), "(null)");
+                }
+                result += buf;
+                break;
+            }
+            case 'd':
+            case 'i':
+            case 'u':
+            case 'x':
+            case 'X':
+            case 'o':
+            case 'p':
+            case 'n':
+            {
+                int value = std::get<int>(args[argIndex++]);
+                char buf[64];
+                snprintf(buf, sizeof(buf), formatSpec.c_str(), value);
+                result += buf;
+                break;
+            }
+            case 'e':
+            case 'f':
+            case 'g':
+            case 'G':
+            {
+                double value = std::get<double>(args[argIndex++]);
+                char buf[128];
+                snprintf(buf, sizeof(buf), formatSpec.c_str(), value);
+                result += buf;
+                break;
+            }
+            default:
+                throw std::invalid_argument("Unsupported format specifier: " + formatSpec);
+            }
+            }
+
+        if (argIndex < args.size()) {
+            throw std::runtime_error("Too many arguments provided");
+        }
+
+        *this = result.c_str();
+    }
+public:
+
+    template<typename... Args>
+    void Format(const std::string& lpszFormat, Args&&... args) {
+        if (lpszFormat.empty()) {
+            throw std::invalid_argument("Invalid format string");
+        }
+
+        std::vector<std::variant<int, double, const char*, const wchar_t*>> convertedArgs;
+        (convertArg(convertedArgs, std::forward<Args>(args)), ...);
+
+        FormatVImpl(lpszFormat, convertedArgs);
+    }
+
+    template<typename... Args>
+    void Format(const char* lpszFormat, Args&&... args) {
+        if (!lpszFormat) {
+            throw std::invalid_argument("Invalid format string");
+        }
+
+        std::vector<std::variant<int, double, const char*, const wchar_t*>> convertedArgs;
+        (convertArg(convertedArgs, std::forward<Args>(args)), ...);
+
+        FormatVImpl(std::string(lpszFormat), convertedArgs);
+    }
+
+    template<typename... Args>
+    void Format(UINT nFormatID, Args&&... args) {
         CString strFormat;
         VERIFY(strFormat.LoadString(nFormatID) != 0);
 
-        va_list argList;
-        va_start(argList, nFormatID);
-        FormatV(strFormat, argList);
-        va_end(argList);
-    }
+        std::vector<std::variant<int, double, const char*, const wchar_t*>> convertedArgs;
+        (convertArg(convertedArgs, std::forward<Args>(args)), ...);
 
+        FormatVImpl(strFormat, convertedArgs);
+    }
 };
 
 //BOOL _PPMFC_API AfxExtractSubString(CString& rString, LPCTSTR lpszFullString,
