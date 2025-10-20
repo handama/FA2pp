265ed02d83bd1e1a23268e9effc01d4098491b2a
handama <55939089+handama@users.noreply.github.com>
2025-10-21 02:26:56 +0800

    destructor of CUpdateProgress

--- CUpdateProgress.h
+++ CUpdateProgress.h
@@ -10,6 +10,9 @@ public:
 	CUpdateProgress(ppmfc::CString lpDescription, ppmfc::CWnd* pParent)
 		{ JMP_THIS(0x4D3A80); }
 
+	~CUpdateProgress()
+		{ JMP_THIS(0x4D3B70); }
+
 	//member properties
 	BOOL m_bCancel;
 	CProgressCtrl ProgressBar;
