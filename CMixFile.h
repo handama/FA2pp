92dc865503300c72d52dd6d8c4ef7719efef8b2b
handama <55939089+handama@users.noreply.github.com>
2026-03-22 22:37:30 +0800

    Update CMixFile.h

--- CMixFile.h
+++ CMixFile.h
@@ -14,6 +14,6 @@ public:
     static int __cdecl Open(const char* pName, int nParentMix) { JMP_STD(0x525470); }
     static bool __cdecl HasFile(const char* pName, int nMix) { JMP_STD(0x525670); }
     static bool __cdecl ExtractFile(const char* pName, const char* pPath, int nMix) { JMP_STD(0x5257D0) }
-    static bool __cdecl LoadSHP(const char* pName, int nMix) { JMP_STD(0x525C50); }
+    static bool __cdecl LoadSHP(const char* pName, int nMix = -1) { JMP_STD(0x525C50); }
     static bool __cdecl SaveMix(const char* pName, const char** pFiles, int nFileCount) { JMP_STD(0x527750); }
 };
